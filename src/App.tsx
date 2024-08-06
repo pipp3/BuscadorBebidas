d098e436241603952eca26163dd90c68b891e881
Felipe Muñoz <felipe@MacBook-Air-de-Felipe.local>
2024-08-05 21:40:44 -0400

    Inicio del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-6xl">Buscador de bebidas</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/views/FavoritesPage.tsx
@@ -0,0 +1,9 @@
+
+
+export default function FavoritesPage() {
+  return (
+    <div>
+      <h1>Favoritos</h1>
+    </div>
+  )
+}
